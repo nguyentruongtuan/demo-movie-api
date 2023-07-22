6b6fa092cfecab48284c959034744652af30e729
TuanNT <tuan@wearebloom.com.au>
2023-07-22 16:18:28 +0700

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,15 @@ import { TYPES } from "@bootstrap/types";
 import bodyParser from "body-parser";
 import { Database } from "./bootstrap/database";
 import { ErrorResponse } from "./middleware/error-response";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
